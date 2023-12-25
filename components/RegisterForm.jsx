d339e3c3786d4d589778779f0612c02ab338f938
Mirfozil <mirfoziln@gmail.com>
2023-12-25 22:39:30 +0500

    init

---
+++ components/RegisterForm.jsx
@@ -0,0 +1,101 @@
+"use client";
+
+import Link from "next/link";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+
+export default function RegisterForm() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const router = useRouter();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (!name || !email || !password) {
+      setError("All fields are necessary.");
+      return;
+    }
+
+    try {
+      const resUserExists = await fetch("api/userExists", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ email }),
+      });
+
+      const { user } = await resUserExists.json();
+
+      if (user) {
+        setError("User already exists.");
+        return;
+      }
+
+      const res = await fetch("api/register", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          name,
+          email,
+          password,
+        }),
+      });
+
+      if (res.ok) {
+        const form = e.target;
+        form.reset();
+        router.push("/");
+      } else {
+        console.log("User registration failed.");
+      }
+    } catch (error) {
+      console.log("Error during registration: ", error);
+    }
+  };
+
+  return (
+    <div className="grid h-screen place-items-center">
+      <div className="p-5 border-t-4 border-green-400 rounded-lg shadow-lg">
+        <h1 className="my-4 text-xl font-bold">Register</h1>
+
+        <form onSubmit={handleSubmit} className="flex flex-col gap-3">
+          <input
+            onChange={(e) => setName(e.target.value)}
+            type="text"
+            placeholder="Full Name"
+          />
+          <input
+            onChange={(e) => setEmail(e.target.value)}
+            type="text"
+            placeholder="Email"
+          />
+          <input
+            onChange={(e) => setPassword(e.target.value)}
+            type="password"
+            placeholder="Password"
+          />
+          <button className="px-6 py-2 font-bold text-white bg-green-600 cursor-pointer">
+            Register
+          </button>
+
+          {error && (
+            <div className="px-3 py-1 mt-2 text-sm text-white bg-red-500 rounded-md w-fit">
+              {error}
+            </div>
+          )}
+
+          <Link className="mt-3 text-sm text-right" href={"/"}>
+            Already have an account? <span className="underline">Login</span>
+          </Link>
+        </form>
+      </div>
+    </div>
+  );
+}
---
+++ components/UserInfo.jsx
@@ -0,0 +1,27 @@
+"use client";
+
+import { signOut } from "next-auth/react";
+import { useSession } from "next-auth/react";
+
+export default function UserInfo() {
+  const { data: session } = useSession();
+
+  return (
+    <div className="grid h-screen place-items-center">
+      <div className="flex flex-col gap-2 p-8 my-6 shadow-lg bg-zince-300/10">
+        <div>
+          Name: <span className="font-bold">{session?.user?.name}</span>
+        </div>
+        <div>
+          Email: <span className="font-bold">{session?.user?.email}</span>
+        </div>
+        <button
+          onClick={() => signOut()}
+          className="px-6 py-2 mt-3 font-bold text-white bg-red-500"
+        >
+          Log Out
+        </button>
+      </div>
+    </div>
+  );
+}
